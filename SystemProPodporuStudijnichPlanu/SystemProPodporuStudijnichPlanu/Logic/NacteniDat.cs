e15d7979572b5ffea63563d2501af18e5c6a16aa
cink01 <43788601+cink01@users.noreply.github.com>
2018-12-21 10:47:45 +0100

    nd

--- SystemProPodporuStudijnichPlanu/SystemProPodporuStudijnichPlanu/Logic/NacteniDat.cs
+++ SystemProPodporuStudijnichPlanu/SystemProPodporuStudijnichPlanu/Logic/NacteniDat.cs
@@ -30,5 +30,30 @@ namespace SystemProPodporuStudijnichPlanu
                 MessageBox.Show("Načtení dat skončilo s chybou: " + ex, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        public void ProvedPopis(string path)
+        {
+            var line = "";
+            var reader = new StreamReader(path);//vytvorení čtení řádku ze souboru
+            DataCrud DC = new DataCrud();
+            try
+            {
+                while (reader.Peek() >= 0)//testovavni zda je jeste dalsi radek
+                {
+                    //precteni radku
+                    line = reader.ReadLine();
+                    string[] fulldata = line.Split(';');//rozlozeni prvku v line do prvku pole fulldata k jednoduššímu přístupu 
+                    //vložení dat do insertu do databaze
+
+                    DC.InsertPredmet(fulldata[2], fulldata[1], fulldata[3], fulldata[6]);
+
+                }
+                MessageBox.Show("Načtení dat do databéze proběhlo úspešně", "Informace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Načtení dat skončilo s chybou: " + ex, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
