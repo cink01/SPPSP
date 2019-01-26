04ac2438c045cc9bae0593116b88b438fff7316b
cink01 <43788601+cink01@users.noreply.github.com>
2019-01-26 09:57:38 +0100

    CUG upravy komponent

--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUGarant.Designer.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUGarant.Designer.cs
@@ -36,8 +36,8 @@
             this.cmb_katedra = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tb_tel = new System.Windows.Forms.TextBox();
+            this.tb_konz = new System.Windows.Forms.TextBox();
             this.tb_id = new System.Windows.Forms.TextBox();
             this.l_email = new System.Windows.Forms.Label();
             this.l_zkr = new System.Windows.Forms.Label();
@@ -81,8 +81,8 @@
             this.gb_garant.Controls.Add(this.bt_ok);
             this.gb_garant.Controls.Add(this.label1);
             this.gb_garant.Controls.Add(this.label2);
-            this.gb_garant.Controls.Add(this.textBox1);
-            this.gb_garant.Controls.Add(this.textBox2);
+            this.gb_garant.Controls.Add(this.tb_tel);
+            this.gb_garant.Controls.Add(this.tb_konz);
             this.gb_garant.Controls.Add(this.tb_id);
             this.gb_garant.Controls.Add(this.l_email);
             this.gb_garant.Controls.Add(this.l_zkr);
@@ -116,34 +116,34 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(542, 18);
+            this.label1.Location = new System.Drawing.Point(509, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 13;
-            this.label1.Text = "email";
+            this.label1.Text = "Konzultační hodiny";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(392, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "jméno";
+            this.label2.Text = "Telefon";
             // 
-            // textBox1
+            // tb_tel
             // 
-            this.textBox1.Location = new System.Drawing.Point(348, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(138, 20);
-            this.textBox1.TabIndex = 10;
+            this.tb_tel.Location = new System.Drawing.Point(348, 32);
+            this.tb_tel.Name = "tb_tel";
+            this.tb_tel.Size = new System.Drawing.Size(138, 20);
+            this.tb_tel.TabIndex = 10;
             // 
-            // textBox2
+            // tb_konz
             // 
-            this.textBox2.Location = new System.Drawing.Point(492, 32);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(138, 20);
-            this.textBox2.TabIndex = 11;
+            this.tb_konz.Location = new System.Drawing.Point(492, 32);
+            this.tb_konz.Name = "tb_konz";
+            this.tb_konz.Size = new System.Drawing.Size(138, 20);
+            this.tb_konz.TabIndex = 11;
             // 
             // tb_id
             // 
@@ -222,8 +222,8 @@
         private System.Windows.Forms.ComboBox cmb_katedra;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tb_tel;
+        private System.Windows.Forms.TextBox tb_konz;
         private System.Windows.Forms.TextBox tb_id;
         private System.Windows.Forms.Label l_email;
         private System.Windows.Forms.Label l_zkr;
