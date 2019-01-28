d77d033fa65208bbadda6260a2a32783b9c5aa61
cink01 <43788601+cink01@users.noreply.github.com>
2019-01-28 20:52:36 +0100

    Vytvořeny přenosky mezi formuláři

--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUGarant.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUGarant.cs
@@ -16,6 +16,36 @@ namespace SystemProPodporuStudijnichPlanu.Aplication
         {
             InitializeComponent();
         }
+        public string Id
+        {
+            get => tb_id.Text;
+            set => tb_id.Text = value;
+        }
+        public string Jmeno
+        {
+            get => tb_jm.Text;
+            set => tb_jm.Text = value;
+        }
+        public string Email
+        {
+            get => tb_email.Text;
+            set => tb_email.Text = value;
+        }
+        public string Tel
+        {
+            get => tb_tel.Text;
+            set => tb_tel.Text = value;
+        }
+        public string Konz
+        {
+            get => tb_konz.Text;
+            set => tb_konz.Text = value;
+        }
+        public int Kat
+        {
+            get => cmb_katedra.SelectedItem.Value;
+            set => cmb_katedra.SelectedItem.Value = value;
+        }
 
         private void Bt_close_Click(object sender, EventArgs e)
         {
--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUPredmet.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCUPredmet.cs
@@ -17,6 +17,83 @@ namespace SystemProPodporuStudijnichPlanu.Aplication
             InitializeComponent();
         }
 
+        public string Nazev
+        {
+            get => tb_nazev.Text;
+            set => tb_nazev.Text = value;
+        }
+        public string Id
+        {
+            get => tb_id.Text;
+            set => tb_id.Text = value;
+        }
+        public string Zkr
+        {
+            get => tb_zkr.Text;
+            set => tb_zkr.Text = value;
+        }
+        public string Orig
+        {
+            get => tb_orig.Text;
+            set => tb_orig.Text = value;
+        }
+        public string Popis
+        {
+            get => rtb_popis.Text;
+            set => rtb_popis.Text = value;
+        }
+        public int Kredit
+        {
+            get => (int)nud_kredit.Value;
+            set => nud_kredit.Value = value;
+        }
+        public int Semestr
+        {
+            get => (int)nud_semestr.Value;
+            set => nud_semestr.Value = value;
+        }
+        public int Prednaska
+        {
+            get => (int)nud_p.Value;
+            set => nud_p.Value = value;
+        }
+        public int Cv 
+        {
+            get => (int)nud_cv.Value;
+            set => nud_cv.Value = value;
+        }
+        public int Cvk
+        {
+            get => (int)nud_cvk.Value;
+            set => nud_cvk.Value = value;
+        }
+        public int Lab 
+        {
+            get => (int)nud_lab.Value;
+            set => nud_lab.Value = value;
+        }
+        public string Zakonceni
+        {
+            get => cb_zakončení.SelectedItem;
+            set => cb_.SelectedItem = value;
+        }
+        public string Povinnost 
+        {
+            get => cb_povinnost.SelectedItem;
+            set => cb_povinnost.SelectedItem = value;
+        }
+        public int Obor 
+        {
+            get => cb_.SelectedItem.Value;
+            set => cb_.SelectedItem.Value = value;
+        }
+        public string Garant
+        {
+            get => cb_garant.SelectedItem;
+            set => cb_garant.SelectedItem = value;
+        }
+
+
         private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
