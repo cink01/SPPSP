f47ee9f34f4fd5130a5e9c2efa92c74dfcb67f66
cink01 <43788601+cink01@users.noreply.github.com>
2019-02-11 15:48:06 +0100

    prinany komponenty pro vyhledavac

--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.Designer.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.Designer.cs
@@ -50,11 +50,20 @@
             this.gb_o = new System.Windows.Forms.GroupBox();
             this.gb_g = new System.Windows.Forms.GroupBox();
             this.gb_p = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmb_povin = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.gb_vyber.SuspendLayout();
             this.gb_k.SuspendLayout();
             this.gb_o.SuspendLayout();
             this.gb_g.SuspendLayout();
             this.gb_p.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // bt_close
@@ -201,38 +210,39 @@
             // 
             // tb_katedraN
             // 
-            this.tb_katedraN.Location = new System.Drawing.Point(6, 46);
+            this.tb_katedraN.Location = new System.Drawing.Point(64, 46);
             this.tb_katedraN.Name = "tb_katedraN";
-            this.tb_katedraN.Size = new System.Drawing.Size(169, 20);
+            this.tb_katedraN.Size = new System.Drawing.Size(111, 20);
             this.tb_katedraN.TabIndex = 16;
             this.tb_katedraN.TextChanged += new System.EventHandler(this.Tb_katedraN_TextChanged);
             // 
             // tb_oborN
             // 
-            this.tb_oborN.Location = new System.Drawing.Point(6, 46);
+            this.tb_oborN.Location = new System.Drawing.Point(64, 46);
             this.tb_oborN.Name = "tb_oborN";
-            this.tb_oborN.Size = new System.Drawing.Size(169, 20);
+            this.tb_oborN.Size = new System.Drawing.Size(111, 20);
             this.tb_oborN.TabIndex = 17;
             this.tb_oborN.TextChanged += new System.EventHandler(this.Tb_oborN_TextChanged);
             // 
             // tb_garantN
             // 
-            this.tb_garantN.Location = new System.Drawing.Point(6, 46);
+            this.tb_garantN.Location = new System.Drawing.Point(64, 46);
             this.tb_garantN.Name = "tb_garantN";
-            this.tb_garantN.Size = new System.Drawing.Size(169, 20);
+            this.tb_garantN.Size = new System.Drawing.Size(111, 20);
             this.tb_garantN.TabIndex = 18;
             this.tb_garantN.TextChanged += new System.EventHandler(this.Tb_garantN_TextChanged);
             // 
             // tb_predmetN
             // 
-            this.tb_predmetN.Location = new System.Drawing.Point(6, 46);
+            this.tb_predmetN.Location = new System.Drawing.Point(64, 46);
             this.tb_predmetN.Name = "tb_predmetN";
-            this.tb_predmetN.Size = new System.Drawing.Size(169, 20);
+            this.tb_predmetN.Size = new System.Drawing.Size(111, 20);
             this.tb_predmetN.TabIndex = 19;
             this.tb_predmetN.TextChanged += new System.EventHandler(this.Tb_predmetN_TextChanged);
             // 
             // gb_k
             // 
+            this.gb_k.Controls.Add(this.label1);
             this.gb_k.Controls.Add(this.cb_katedra);
             this.gb_k.Controls.Add(this.tb_katedraN);
             this.gb_k.Location = new System.Drawing.Point(12, 63);
@@ -240,10 +250,11 @@
             this.gb_k.Size = new System.Drawing.Size(182, 199);
             this.gb_k.TabIndex = 20;
             this.gb_k.TabStop = false;
-            this.gb_k.Text = "Výběr a vybledávání kateder";
+            this.gb_k.Text = "Výběr a vyhledávání kateder";
             // 
             // gb_o
             // 
+            this.gb_o.Controls.Add(this.label2);
             this.gb_o.Controls.Add(this.cb_obor);
             this.gb_o.Controls.Add(this.tb_oborN);
             this.gb_o.Location = new System.Drawing.Point(193, 63);
@@ -251,10 +262,11 @@
             this.gb_o.Size = new System.Drawing.Size(182, 199);
             this.gb_o.TabIndex = 21;
             this.gb_o.TabStop = false;
-            this.gb_o.Text = "Výběr a vybledávání oborů";
+            this.gb_o.Text = "Výběr a vyhledávání oborů";
             // 
             // gb_g
             // 
+            this.gb_g.Controls.Add(this.label3);
             this.gb_g.Controls.Add(this.cb_garant);
             this.gb_g.Controls.Add(this.tb_garantN);
             this.gb_g.Location = new System.Drawing.Point(374, 63);
@@ -262,10 +274,15 @@
             this.gb_g.Size = new System.Drawing.Size(182, 199);
             this.gb_g.TabIndex = 22;
             this.gb_g.TabStop = false;
-            this.gb_g.Text = "Výběr a vybledávání garantů";
+            this.gb_g.Text = "Výběr a vyhledávání garantů";
             // 
             // gb_p
             // 
+            this.gb_p.Controls.Add(this.numericUpDown1);
+            this.gb_p.Controls.Add(this.label6);
+            this.gb_p.Controls.Add(this.label5);
+            this.gb_p.Controls.Add(this.label4);
+            this.gb_p.Controls.Add(this.cmb_povin);
             this.gb_p.Controls.Add(this.cb_predmet);
             this.gb_p.Controls.Add(this.tb_predmetN);
             this.gb_p.Location = new System.Drawing.Point(555, 63);
@@ -273,7 +290,81 @@
             this.gb_p.Size = new System.Drawing.Size(182, 199);
             this.gb_p.TabIndex = 23;
             this.gb_p.TabStop = false;
-            this.gb_p.Text = "Výběr a vybledávání předmětů";
+            this.gb_p.Text = "Výběr a vyhledávání předmětů";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Název:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Název:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Jméno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Název:";
+            // 
+            // cmb_povin
+            // 
+            this.cmb_povin.FormattingEnabled = true;
+            this.cmb_povin.Location = new System.Drawing.Point(64, 72);
+            this.cmb_povin.Name = "cmb_povin";
+            this.cmb_povin.Size = new System.Drawing.Size(111, 21);
+            this.cmb_povin.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Povinnost:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Semestr:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(64, 99);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(111, 20);
+            this.numericUpDown1.TabIndex = 23;
             // 
             // FormCRUDmidstage
             // 
@@ -305,6 +396,7 @@
             this.gb_g.PerformLayout();
             this.gb_p.ResumeLayout(false);
             this.gb_p.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -332,5 +424,13 @@
         private System.Windows.Forms.GroupBox gb_o;
         private System.Windows.Forms.GroupBox gb_g;
         private System.Windows.Forms.GroupBox gb_p;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmb_povin;
     }
 }
