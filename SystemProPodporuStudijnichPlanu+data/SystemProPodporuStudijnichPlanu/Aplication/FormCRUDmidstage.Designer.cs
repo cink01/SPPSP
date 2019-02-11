cf492f4867163a5bcc6e1853f92b766c97097b6e
cink01 <43788601+cink01@users.noreply.github.com>
2019-02-11 09:36:06 +0100

    příprava na vyhledávání pro správce pozdeji mozna funnkce vyuzit pro listboxy v pridavani(moznost vyhledat podle nazvu/semestu atd)

--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.Designer.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.Designer.cs
@@ -41,7 +41,20 @@
             this.rb_garant = new System.Windows.Forms.RadioButton();
             this.gb_vyber = new System.Windows.Forms.GroupBox();
             this.cb_garant = new System.Windows.Forms.ComboBox();
+            this.cb_predmet = new System.Windows.Forms.ComboBox();
+            this.tb_katedraN = new System.Windows.Forms.TextBox();
+            this.tb_oborN = new System.Windows.Forms.TextBox();
+            this.tb_garantN = new System.Windows.Forms.TextBox();
+            this.tb_predmetN = new System.Windows.Forms.TextBox();
+            this.gb_k = new System.Windows.Forms.GroupBox();
+            this.gb_o = new System.Windows.Forms.GroupBox();
+            this.gb_g = new System.Windows.Forms.GroupBox();
+            this.gb_p = new System.Windows.Forms.GroupBox();
             this.gb_vyber.SuspendLayout();
+            this.gb_k.SuspendLayout();
+            this.gb_o.SuspendLayout();
+            this.gb_g.SuspendLayout();
+            this.gb_p.SuspendLayout();
             this.SuspendLayout();
             // 
             // bt_close
@@ -50,7 +63,7 @@
             this.bt_close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.bt_close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bt_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.bt_close.Location = new System.Drawing.Point(713, 12);
+            this.bt_close.Location = new System.Drawing.Point(662, 268);
             this.bt_close.Name = "bt_close";
             this.bt_close.Size = new System.Drawing.Size(75, 23);
             this.bt_close.TabIndex = 3;
@@ -60,10 +73,10 @@
             // 
             // bt_smazat
             // 
-            this.bt_smazat.BackColor = System.Drawing.Color.Linen;
+            this.bt_smazat.BackColor = System.Drawing.Color.Bisque;
             this.bt_smazat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bt_smazat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.bt_smazat.Location = new System.Drawing.Point(174, 12);
+            this.bt_smazat.Location = new System.Drawing.Point(174, 268);
             this.bt_smazat.Name = "bt_smazat";
             this.bt_smazat.Size = new System.Drawing.Size(75, 23);
             this.bt_smazat.TabIndex = 4;
@@ -72,10 +85,10 @@
             // 
             // bt_novy
             // 
-            this.bt_novy.BackColor = System.Drawing.Color.Honeydew;
+            this.bt_novy.BackColor = System.Drawing.Color.GreenYellow;
             this.bt_novy.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bt_novy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.bt_novy.Location = new System.Drawing.Point(12, 12);
+            this.bt_novy.Location = new System.Drawing.Point(12, 268);
             this.bt_novy.Name = "bt_novy";
             this.bt_novy.Size = new System.Drawing.Size(75, 23);
             this.bt_novy.TabIndex = 5;
@@ -85,10 +98,10 @@
             // 
             // bt_upravit
             // 
-            this.bt_upravit.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.bt_upravit.BackColor = System.Drawing.Color.LemonChiffon;
             this.bt_upravit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bt_upravit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.bt_upravit.Location = new System.Drawing.Point(93, 12);
+            this.bt_upravit.Location = new System.Drawing.Point(93, 268);
             this.bt_upravit.Name = "bt_upravit";
             this.bt_upravit.Size = new System.Drawing.Size(75, 23);
             this.bt_upravit.TabIndex = 6;
@@ -98,7 +111,7 @@
             // cb_katedra
             // 
             this.cb_katedra.FormattingEnabled = true;
-            this.cb_katedra.Location = new System.Drawing.Point(12, 166);
+            this.cb_katedra.Location = new System.Drawing.Point(6, 19);
             this.cb_katedra.Name = "cb_katedra";
             this.cb_katedra.Size = new System.Drawing.Size(169, 21);
             this.cb_katedra.Sorted = true;
@@ -107,7 +120,7 @@
             // cb_obor
             // 
             this.cb_obor.FormattingEnabled = true;
-            this.cb_obor.Location = new System.Drawing.Point(12, 193);
+            this.cb_obor.Location = new System.Drawing.Point(6, 19);
             this.cb_obor.Name = "cb_obor";
             this.cb_obor.Size = new System.Drawing.Size(169, 21);
             this.cb_obor.Sorted = true;
@@ -116,7 +129,7 @@
             // rb_predmet
             // 
             this.rb_predmet.AutoSize = true;
-            this.rb_predmet.Location = new System.Drawing.Point(18, 65);
+            this.rb_predmet.Location = new System.Drawing.Point(599, 19);
             this.rb_predmet.Name = "rb_predmet";
             this.rb_predmet.Size = new System.Drawing.Size(65, 17);
             this.rb_predmet.TabIndex = 9;
@@ -127,7 +140,7 @@
             // rb_obor
             // 
             this.rb_obor.AutoSize = true;
-            this.rb_obor.Location = new System.Drawing.Point(18, 19);
+            this.rb_obor.Location = new System.Drawing.Point(244, 19);
             this.rb_obor.Name = "rb_obor";
             this.rb_obor.Size = new System.Drawing.Size(48, 17);
             this.rb_obor.TabIndex = 10;
@@ -138,7 +151,7 @@
             // rb_katedra
             // 
             this.rb_katedra.AutoSize = true;
-            this.rb_katedra.Location = new System.Drawing.Point(18, 42);
+            this.rb_katedra.Location = new System.Drawing.Point(18, 19);
             this.rb_katedra.Name = "rb_katedra";
             this.rb_katedra.Size = new System.Drawing.Size(62, 17);
             this.rb_katedra.TabIndex = 11;
@@ -149,7 +162,7 @@
             // rb_garant
             // 
             this.rb_garant.AutoSize = true;
-            this.rb_garant.Location = new System.Drawing.Point(18, 88);
+            this.rb_garant.Location = new System.Drawing.Point(418, 19);
             this.rb_garant.Name = "rb_garant";
             this.rb_garant.Size = new System.Drawing.Size(57, 17);
             this.rb_garant.TabIndex = 12;
@@ -163,9 +176,9 @@
             this.gb_vyber.Controls.Add(this.rb_garant);
             this.gb_vyber.Controls.Add(this.rb_predmet);
             this.gb_vyber.Controls.Add(this.rb_katedra);
-            this.gb_vyber.Location = new System.Drawing.Point(12, 41);
+            this.gb_vyber.Location = new System.Drawing.Point(12, 12);
             this.gb_vyber.Name = "gb_vyber";
-            this.gb_vyber.Size = new System.Drawing.Size(169, 119);
+            this.gb_vyber.Size = new System.Drawing.Size(725, 45);
             this.gb_vyber.TabIndex = 13;
             this.gb_vyber.TabStop = false;
             this.gb_vyber.Text = "Vyberte s čím chcete pracovat";
@@ -173,10 +186,94 @@
             // cb_garant
             // 
             this.cb_garant.FormattingEnabled = true;
-            this.cb_garant.Location = new System.Drawing.Point(12, 220);
+            this.cb_garant.Location = new System.Drawing.Point(6, 19);
             this.cb_garant.Name = "cb_garant";
             this.cb_garant.Size = new System.Drawing.Size(169, 21);
             this.cb_garant.TabIndex = 14;
+            // 
+            // cb_predmet
+            // 
+            this.cb_predmet.FormattingEnabled = true;
+            this.cb_predmet.Location = new System.Drawing.Point(6, 19);
+            this.cb_predmet.Name = "cb_predmet";
+            this.cb_predmet.Size = new System.Drawing.Size(169, 21);
+            this.cb_predmet.TabIndex = 15;
+            // 
+            // tb_katedraN
+            // 
+            this.tb_katedraN.Location = new System.Drawing.Point(6, 46);
+            this.tb_katedraN.Name = "tb_katedraN";
+            this.tb_katedraN.Size = new System.Drawing.Size(169, 20);
+            this.tb_katedraN.TabIndex = 16;
+            this.tb_katedraN.TextChanged += new System.EventHandler(this.Tb_katedraN_TextChanged);
+            // 
+            // tb_oborN
+            // 
+            this.tb_oborN.Location = new System.Drawing.Point(6, 46);
+            this.tb_oborN.Name = "tb_oborN";
+            this.tb_oborN.Size = new System.Drawing.Size(169, 20);
+            this.tb_oborN.TabIndex = 17;
+            this.tb_oborN.TextChanged += new System.EventHandler(this.Tb_oborN_TextChanged);
+            // 
+            // tb_garantN
+            // 
+            this.tb_garantN.Location = new System.Drawing.Point(6, 46);
+            this.tb_garantN.Name = "tb_garantN";
+            this.tb_garantN.Size = new System.Drawing.Size(169, 20);
+            this.tb_garantN.TabIndex = 18;
+            this.tb_garantN.TextChanged += new System.EventHandler(this.Tb_garantN_TextChanged);
+            // 
+            // tb_predmetN
+            // 
+            this.tb_predmetN.Location = new System.Drawing.Point(6, 46);
+            this.tb_predmetN.Name = "tb_predmetN";
+            this.tb_predmetN.Size = new System.Drawing.Size(169, 20);
+            this.tb_predmetN.TabIndex = 19;
+            this.tb_predmetN.TextChanged += new System.EventHandler(this.Tb_predmetN_TextChanged);
+            // 
+            // gb_k
+            // 
+            this.gb_k.Controls.Add(this.cb_katedra);
+            this.gb_k.Controls.Add(this.tb_katedraN);
+            this.gb_k.Location = new System.Drawing.Point(12, 63);
+            this.gb_k.Name = "gb_k";
+            this.gb_k.Size = new System.Drawing.Size(182, 199);
+            this.gb_k.TabIndex = 20;
+            this.gb_k.TabStop = false;
+            this.gb_k.Text = "Výběr a vybledávání kateder";
+            // 
+            // gb_o
+            // 
+            this.gb_o.Controls.Add(this.cb_obor);
+            this.gb_o.Controls.Add(this.tb_oborN);
+            this.gb_o.Location = new System.Drawing.Point(193, 63);
+            this.gb_o.Name = "gb_o";
+            this.gb_o.Size = new System.Drawing.Size(182, 199);
+            this.gb_o.TabIndex = 21;
+            this.gb_o.TabStop = false;
+            this.gb_o.Text = "Výběr a vybledávání oborů";
+            // 
+            // gb_g
+            // 
+            this.gb_g.Controls.Add(this.cb_garant);
+            this.gb_g.Controls.Add(this.tb_garantN);
+            this.gb_g.Location = new System.Drawing.Point(374, 63);
+            this.gb_g.Name = "gb_g";
+            this.gb_g.Size = new System.Drawing.Size(182, 199);
+            this.gb_g.TabIndex = 22;
+            this.gb_g.TabStop = false;
+            this.gb_g.Text = "Výběr a vybledávání garantů";
+            // 
+            // gb_p
+            // 
+            this.gb_p.Controls.Add(this.cb_predmet);
+            this.gb_p.Controls.Add(this.tb_predmetN);
+            this.gb_p.Location = new System.Drawing.Point(555, 63);
+            this.gb_p.Name = "gb_p";
+            this.gb_p.Size = new System.Drawing.Size(182, 199);
+            this.gb_p.TabIndex = 23;
+            this.gb_p.TabStop = false;
+            this.gb_p.Text = "Výběr a vybledávání předmětů";
             // 
             // FormCRUDmidstage
             // 
@@ -184,11 +281,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.CancelButton = this.bt_close;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cb_garant);
+            this.ClientSize = new System.Drawing.Size(750, 305);
+            this.Controls.Add(this.gb_p);
+            this.Controls.Add(this.gb_g);
+            this.Controls.Add(this.gb_o);
+            this.Controls.Add(this.gb_k);
             this.Controls.Add(this.gb_vyber);
-            this.Controls.Add(this.cb_obor);
-            this.Controls.Add(this.cb_katedra);
             this.Controls.Add(this.bt_upravit);
             this.Controls.Add(this.bt_novy);
             this.Controls.Add(this.bt_smazat);
@@ -196,8 +294,17 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCRUDmidstage";
             this.Text = "FormCRUDmidstage";
+            this.Load += new System.EventHandler(this.FormCRUDmidstage_Load);
             this.gb_vyber.ResumeLayout(false);
             this.gb_vyber.PerformLayout();
+            this.gb_k.ResumeLayout(false);
+            this.gb_k.PerformLayout();
+            this.gb_o.ResumeLayout(false);
+            this.gb_o.PerformLayout();
+            this.gb_g.ResumeLayout(false);
+            this.gb_g.PerformLayout();
+            this.gb_p.ResumeLayout(false);
+            this.gb_p.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -216,5 +323,14 @@
         private System.Windows.Forms.RadioButton rb_garant;
         private System.Windows.Forms.GroupBox gb_vyber;
         private System.Windows.Forms.ComboBox cb_garant;
+        private System.Windows.Forms.ComboBox cb_predmet;
+        private System.Windows.Forms.TextBox tb_katedraN;
+        private System.Windows.Forms.TextBox tb_oborN;
+        private System.Windows.Forms.TextBox tb_garantN;
+        private System.Windows.Forms.TextBox tb_predmetN;
+        private System.Windows.Forms.GroupBox gb_k;
+        private System.Windows.Forms.GroupBox gb_o;
+        private System.Windows.Forms.GroupBox gb_g;
+        private System.Windows.Forms.GroupBox gb_p;
     }
 }
--- SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.cs
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Aplication/FormCRUDmidstage.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Data.Common;
-using System.Data;
-using System.Data.SqlClient;
+using SystemProPodporuStudijnichPlanu.Logic;
+using System.Collections.Generic;
 
 namespace SystemProPodporuStudijnichPlanu.Aplication
 {
     public partial class FormCRUDmidstage : Form
     {
+        public List<Katedra> kat = new List<Katedra>();
+        public List<Obor> obo = new List<Obor>();
+        public List<Garant> gar = new List<Garant>();
+        public List<Predmet> pre = new List<Predmet>();
+        public StringComparison Comp { get; set; } = StringComparison.OrdinalIgnoreCase;
         public FormCRUDmidstage()
         {
             InitializeComponent();
@@ -16,17 +20,27 @@ namespace SystemProPodporuStudijnichPlanu.Aplication
             a.FillKatedraCB(cb_katedra);
             a.FillGarantCB(cb_garant);
         }
-
         private void Bt_novy_Click(object sender, EventArgs e)
         {
             if (rb_garant.Checked == true)
+            {
+                //  bt_novy;
                 NewGarant();
+            }
             if (rb_predmet.Checked == true)
+            {
                 NewPredmet();
+            }
+
             if (rb_obor.Checked == true)
+            {
                 NewObor();
+            }
+
             if (rb_katedra.Checked == true)
+            {
                 NewKatedra();
+            }
         }
         private void NewGarant()
         {
@@ -138,5 +152,66 @@ namespace SystemProPodporuStudijnichPlanu.Aplication
             }
         }
         private void Bt_close_Click(object sender, EventArgs e) => Close();
+        private void FormCRUDmidstage_Load(object sender, EventArgs e)
+        {
+
+        }
+        //vraceni listu a plnění datasetu a mazani z nich
+        private void Tb_katedraN_TextChanged(object sender, EventArgs e)
+        {
+            cb_katedra.Items.Clear();
+            //chb_exist.Checked = false;
+            cb_katedra.Text = "Nalezené katedry";
+            foreach (Katedra k in kat)
+            {
+                if (k.Naz_k.IndexOf(tb_katedraN.Text, Comp) >= 0)
+                {
+                    cb_katedra.Items.Add(k.Naz_k);
+                    //  chb_exist.Checked = true;
+                }
+            }
+        }
+        private void Tb_oborN_TextChanged(object sender, EventArgs e)
+        {
+            cb_obor.Items.Clear();
+            //chb_exist.Checked = false;
+            cb_obor.Text = "Nalezené obory";
+            foreach (Obor o in obo)
+            {
+                if (o.Name_obor.IndexOf(tb_oborN.Text, Comp) >= 0)
+                {
+                    cb_obor.Items.Add(o.Name_obor);
+                    //  chb_exist.Checked = true;
+                }
+            }
+        }
+        private void Tb_garantN_TextChanged(object sender, EventArgs e)
+        {
+            cb_garant.Items.Clear();
+            //chb_exist.Checked = false;
+            cb_garant.Text = "Nalezení garanti";
+            foreach (Garant g in gar)
+            {
+                if (g.Jmeno_v.IndexOf(tb_garantN.Text, Comp) >= 0)
+                {
+                    cb_garant.Items.Add(g.Jmeno_v);
+                    //  chb_exist.Checked = true;
+                }
+            }
+        }
+        private void Tb_predmetN_TextChanged(object sender, EventArgs e)
+        {
+            cb_predmet.Items.Clear();
+            //chb_exist.Checked = false;
+            cb_predmet.Text = "Nalezené předměty";
+            foreach (Predmet p in pre)
+            {
+                if (p.Name_predmet.IndexOf(tb_predmetN.Text, Comp) >= 0)
+                {
+                    cb_predmet.Items.Add(p.Name_predmet);
+                    //  chb_exist.Checked = true;
+                }
+            }
+        }
     }
 }
