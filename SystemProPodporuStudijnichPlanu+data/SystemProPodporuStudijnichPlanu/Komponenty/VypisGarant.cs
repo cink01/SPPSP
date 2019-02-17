45135622806ca971fa5ac23e3d850cc110c510a1
cink01 <43788601+cink01@users.noreply.github.com>
2019-02-17 20:11:24 +0100

    komponenta pro garanta(detail) vymazaní uvozovek a specialních znaků ze souborů

---
+++ SystemProPodporuStudijnichPlanu+data/SystemProPodporuStudijnichPlanu/Komponenty/VypisGarant.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+
+namespace SystemProPodporuStudijnichPlanu.Komponenty
+{
+    public partial class VypisGarant : UserControl
+    {
+        public VypisGarant()
+        {
+            InitializeComponent();
+        }
+        public Garant G { get; set; }
+        public string Konzultace
+        {
+            get => tb_konz.Text;
+            set => tb_konz.Text = value;
+        }
+        public string Katedra
+        {
+            get => tb_kat.Text;
+            set => tb_kat.Text = value;
+        }
+        public string Telefon
+        {
+            get => tb_tel.Text;
+            set => tb_tel.Text = value;
+        }
+        public string Email
+        {
+            get => tb_email.Text;
+            set => tb_email.Text = value;
+        }
+    }
+}
